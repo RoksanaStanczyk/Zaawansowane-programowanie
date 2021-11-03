72391c4e9edb99d0aa9e9b6b10e68ef56b733b04
RoksanaStanczyk <58743872+RoksanaStanczyk@users.noreply.github.com>
2021-11-03 16:02:35 +0100

    PEP8

--- Zaawansowane_programowanie_cw2/main.py
+++ Zaawansowane_programowanie_cw2/main.py
@@ -1,36 +1,43 @@
 # zad 2a
-# names_list = ["A", "B", "C", "D", "E"]
-#
-# def print_names (list = []):
-#     for i in list:
-#         print(i)
-#
-# print_names(names_list)
+names_list = ["A", "B", "C", "D", "E"]
+
+
+def print_names(list=[]):
+    for i in list:
+        print(i)
+
+
+print_names(names_list)
 
 # zad 2b
-# num_list = [1, 2, 3, 4, 5]
-# new_num_list = []
-# def print_num(list = []):
-#     for i in list:
-#         num = i*2
-#         new_num_list.append(num)
-#     print(new_num_list)
-#
-# def print_num2(list = []):
-#     num = [i * 2 for i in list]
-#     new_num_list.append(num)
-#     print(new_num_list)
-#
-# print_num2(num_list)
+num_list = [1, 2, 3, 4, 5]
+new_num_list = []
+
+
+def print_num(list=[]):
+    for i in list:
+        num = i * 2
+        new_num_list.append(num)
+    print(new_num_list)
+
+
+def print_num2(list=[]):
+    num = [i * 2 for i in list]
+    new_num_list.append(num)
+    print(new_num_list)
+
+
+print_num2(num_list)
 
 # zad 2c
-# for i in range(10):
-#     if i % 2 ==0:
-#         print(i)
+for i in range(10):
+    if i % 2 == 0:
+        print(i)
 
 
 # zad 2d
-for i in range(0,10,2):
+for i in range(0, 10, 2):
     print(i)
+
 
 
