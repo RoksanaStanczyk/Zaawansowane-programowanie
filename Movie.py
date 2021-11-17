d9c3ec9d55fa395a2fa0563f40f7e968b2a30875
RoksanaStanczyk <58743872+RoksanaStanczyk@users.noreply.github.com>
2021-11-17 15:01:27 +0100

    Add files via upload

---
+++ Movie.py
@@ -0,0 +1,6 @@
+class Movie:
+    def __init__(self, id, title, genres):
+        self.id = id
+        self.title = title
+        self.genres = genres
+
---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from read_csv import read
+app = Flask(__name__)
+api = Api(app)
+
+# path = open('movies.csv', encoding="utf8")
+path = 'movies.csv'
+class HelloWorld(Resource):
+    def get(self):
+        return read(path)
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
